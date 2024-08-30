40196b925826d52156f12f53c7dae706e2fb7c27
Wendel Vinicius <133383757+wendell67@users.noreply.github.com>
2024-08-30 14:52:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let primeiroNome = "João";
+let sobrenome = "Silva"; 
+let idade = 30;
+let altura = 1.75;
+let massa = 70;
+let imc = massa / (altura * altura);
+
+
+console.log(`Paciente: ${nome}
+${sobrenome}, idade: ${idade}, altura:
+${altura.toFixed(2)}, IMC:
+${imc.toFixed(2)}`);
+
